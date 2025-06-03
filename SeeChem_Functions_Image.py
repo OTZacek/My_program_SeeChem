deb2297be328d148f07ca432352abcc426580a43
Isaac Zhu <Xibo13425@gmail.com>
2025-06-04 10:49:25 +1200

    .

--- SeeChem_Functions_Image.py
+++ SeeChem_Functions_Image.py
@@ -1,6 +1,4 @@
 from PIL import Image, ImageTk
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
 
 # image
 def Load_png(file_path, width = None):
@@ -10,13 +8,3 @@ def Load_png(file_path, width = None):
     height = int(original.height * ratio)
     resized = original.resize((width, height), Image.LANCZOS)
     return ImageTk.PhotoImage(resized)
-
-def Load_svg(file_path, width=None):
-    drawing = svg2rlg(file_path)  
-    if width and drawing.width != 0:
-        scale_factor = width / drawing.width
-        img_pil = renderPM.drawToPIL(drawing, scale=scale_factor)
-    else:
-        img_pil = renderPM.drawToPIL(drawing)
-    
-    return ImageTk.PhotoImage(img_pil)
